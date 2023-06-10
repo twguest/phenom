d42073f4c9cb0266f86b2bb696e5f9485d925dad
Trey Guest <twguest@students.latrobe.edu.au>
2023-06-10 17:24:48 +0200

    Add basic Test

---
+++ phenom/spectra.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 10 17:13:06 2023
+
+This script handles construction of a spectra
+
+@author: twguest
+"""
+import numpy as np
+
+def flat_spectra(n = 50):
+    """
+    function for test purposes only
+    """
+    return np.ones(n)
+    
