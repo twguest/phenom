631e5640f5b4d6fd2437bafc4d555344631b876b
twguest <trey.guest@xfel.eu>
2023-06-15 17:35:34 +0200

    Try Git

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'X.Y.Z.post24.dev0+g8592b51',
+    VERSION: 'X.Y.Z.post28.dev0+gac0a714',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
