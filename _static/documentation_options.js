9a0cb7c8b5cda1b9da26a2b89b0dca594d404911
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2023-07-07 08:14:39 +0000

    Update docs after building Travis build 50 of twguest/phenom
    The docs were built from the branch 'master' against the commit
    eabeedba8e9e80936c705f07745ee4e1e141608e.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/twguest/phenom/jobs/605623166.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.9.15/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post46.dev0+g61a471a',
+    VERSION: '0.0.1.post61.dev0+g7c5302a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
