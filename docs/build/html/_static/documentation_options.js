fef52d711da3961873d79e7cbe29d90766558a38
Trey Guest <twguest@students.latrobe.edu.au>
2023-07-08 22:21:20 +0200

    Docs Fix.

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.post77.dev0+g4264519',
+    VERSION: '0.0.3.post2+gcf568cb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
